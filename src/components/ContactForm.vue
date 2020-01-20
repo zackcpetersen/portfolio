fd1ee30ae850dc58ebcc7b85cb1258d4e6e79ef1
n3rdn1nja <zackcpetersen@gmail.com>
2020-01-20 00:08:20 -0700

    adding progress bar to form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -12,42 +12,58 @@
                 required
         />
         <v-expand-transition>
-        <v-text-field
-                v-show="[2, 3, 4].includes(step)"
-                v-model="fields.email"
-                :rules="fields.emailRules"
-                label="E-mail"
-                required
-        />
-        </v-expand-transition>
-        <v-expand-transition>
-        <v-text-field
-                v-show="[3, 4].includes(step)"
-                v-model="fields.phone"
-                :rules="fields.phoneRules"
-                label="Phone"
-        />
-        </v-expand-transition>
-        <v-expand-transition>
-        <div v-show="[4].includes(step)">
-            <v-textarea
-                    outlined
-                    v-model="fields.message"
-                    name="input-7-1"
-                    :rules="fields.messageRules"
-                    label="Message"
+            <v-text-field
+                    v-show="[2, 3, 4].includes(step)"
+                    v-model="fields.email"
+                    :rules="fields.emailRules"
+                    label="E-mail"
                     required
             />
-            <v-btn
-                    :disabled="!valid"
-                    color="success"
-                    class="mr-4"
-                    @click="validate"
-            >
-                Send Message
-            </v-btn>
-        </div>
         </v-expand-transition>
+        <v-expand-transition>
+            <v-text-field
+                    v-show="[3, 4].includes(step)"
+                    v-model="fields.phone"
+                    :rules="fields.phoneRules"
+                    label="Phone"
+            />
+        </v-expand-transition>
+        <v-expand-transition>
+            <div v-show="[4].includes(step)">
+                <v-textarea
+                        outlined
+                        v-model="fields.message"
+                        name="input-7-1"
+                        :rules="fields.messageRules"
+                        label="Message"
+                        required
+                />
+                <v-btn
+                        :disabled="!valid"
+                        color="success"
+                        class="mr-4"
+                        @click="validate"
+                >
+                    Send Message
+                </v-btn>
+            </div>
+        </v-expand-transition>
+        <v-progress-linear
+                :active="progress.active"
+                :background-opacity="progress.opacity"
+                :bottom="progress.bottom"
+                :buffer-value="progress.buffer"
+                :height="progress.height"
+                :indeterminate="progress.indeterminate"
+                :query="progress.query"
+                :rounded="progress.rounded"
+                :stream="progress.stream"
+                :striped="progress.striped"
+                :top="progress.top"
+                :value="progress.value"
+                color="grey"
+                class="mt-10"
+        />
     </v-form>
 
 </template>
@@ -66,6 +82,7 @@
                         v => {
                             if (!!v && v.length <= 30) {
                                 this.step = 2
+                                this.progress.value = 25
                                 return true
                             }
                             return false
@@ -78,6 +95,7 @@
                         v => {
                             if (!!v && /.+@.+\..+/.test(v)) {
                                 this.step = 3
+                                this.progress.value = 50
                                 return true
                             }
                             return false
@@ -89,6 +107,7 @@
                         v => {
                             if (!!v && /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v)) {
                                 this.step = 4
+                                this.progress.value = 75
                                 return true
                             }
                             return false
@@ -97,7 +116,30 @@
                     message: '',
                     messageRules: [
                         v => !!v || 'Message is required',
+                        v => {
+                            if (!!v) {
+                                this.progress.value = 100
+                                return true
+                            }
+                            return false
+                        }
                     ],
+                },
+                progress: {
+                    absolute: false,
+                    active: true,
+                    opacity: 0.3,
+                    bottom: false,
+                    buffer: 100,
+                    fixed: false,
+                    height: 4,
+                    indeterminate: false,
+                    query: false,
+                    rounded: false,
+                    stream: false,
+                    striped: false,
+                    top: false,
+                    value: 0,
                 },
                 lazy: false,
                 step: 1,

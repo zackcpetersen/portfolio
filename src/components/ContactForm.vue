a9709d4f68200ca745cd772dfcfe566c8dbbfeca
n3rdn1nja <zackcpetersen@gmail.com>
2020-01-19 23:31:19 -0700

    form multistep

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,30 +1,37 @@
 <template>
-        <v-form
-                ref="form"
-                v-model="valid"
-                :lazy-validation="lazy">
-            <v-text-field
-                    v-model="name"
-                    :counter="30"
-                    :rules="nameRules"
-                    label="Name"
-                    required
-            />
-            <v-text-field
-                    v-model="email"
-                    :rules="emailRules"
-                    label="E-mail"
-                    required
-            />
-            <v-text-field
-                    v-model="phone"
-
-                    label="Phone"
-            />
+    <v-form
+            ref="form"
+            v-model="valid"
+            :lazy-validation="lazy">
+        <v-text-field
+                v-show="[1, 2, 3, 4].includes(step)"
+                v-model="fields.name"
+                :counter="30"
+                :rules="fields.nameRules"
+                label="Name"
+                required
+        />
+        <v-text-field
+                v-show="[2, 3, 4].includes(step)"
+                v-model="fields.email"
+                :rules="fields.emailRules"
+                label="E-mail"
+                required
+        />
+        <v-text-field
+                v-show="[3, 4].includes(step)"
+                v-model="fields.phone"
+                :rules="fields.phoneRules"
+                label="Phone"
+        />
+        <div v-show="[4].includes(step)">
             <v-textarea
                     outlined
+                    v-model="fields.message"
                     name="input-7-1"
+                    :rules="fields.messageRules"
                     label="Message"
+                    required
             />
             <v-btn
                     :disabled="!valid"
@@ -34,7 +41,8 @@
             >
                 Send Message
             </v-btn>
-        </v-form>
+        </div>
+    </v-form>
 
 </template>
 
@@ -44,28 +52,53 @@
         data() {
             return {
                 valid: true,
-                name: '',
-                nameRules: [
-                    v => !!v || 'Name is required',
-                    v => (v && v.length <= 30) || 'Name must be less than 30 characters',
-                ],
-                email: '',
-                emailRules: [
-                    v => !!v || 'E-mail is required',
-                    v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-                ],
-                phone: '',
-                message: '',
-                feedback: [],
-                lazy: false
+                fields: {
+                    name: '',
+                    nameRules: [
+                        v => !!v || 'Name is required',
+                        v => (v && v.length <= 30) || 'Name must be less than 30 characters',
+                        v => {
+                            if (!!v && v.length <= 30) {
+                                this.step = 2
+                            }
+                        }
+                    ],
+                    email: '',
+                    emailRules: [
+                        v => !!v || 'E-mail is required',
+                        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+                        v => {
+                            if (!!v && /.+@.+\..+/.test(v)) {
+                                this.step = 3
+                            }
+                        }
+                    ],
+                    phone: '',
+                    phoneRules: [
+                        v => /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v) || 'Invalid Phone Number',
+                        v => {
+                            if (!!v && /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v)) {
+                                this.step = 4
+                            }
+                        }
+                    ],
+                    message: '',
+                    messageRules: [
+                        v => !!v || 'Message is required',
+                    ],
+                },
+                lazy: false,
+                step: 1,
             }
         },
         methods: {
             validate() {
                 if (this.$refs.form.validate()) {
-                    this.snackbar = true
+                    alert('You Did It!')
+                    // what to do on form valid
+                    // send data to firebase
                 }
-            }
+            },
         }
     }
 </script>

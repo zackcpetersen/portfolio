e8c5ced80d10e8eb705dfa6ef6315910d7d4d748
n3rdn1nja <zackcpetersen@gmail.com>
2020-01-23 19:33:25 -0700

    adding buttons to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,20 @@
 <template>
     <v-container>
-        <v-footer
-                :padless="padless"
-        >
-            <v-card
-                    flat
-                    tile
-                    width="100%"
-                    class="text-center"
-                    color="rgba(0, 0, 0, 0.0)"
-            >
-                <p><strong>Let's Talk!</strong></p>
-                <p>Wanna get in touch? Feel Free to contact me at
-                    <br><strong>- <a href="mailto:zackcpetersen@gmail.com">zackcpetersen@gmail.com</a></strong>
-                    <br><strong>- <a href="tel:3854045953">(385) 404-5953</a></strong>
-                    <br>or leave me a message on the <strong>
-                        <router-link :to="{ name: 'Contact' }" class="link">contact page!
-                        </router-link>
-                    </strong></p>
-            </v-card>
-        </v-footer>
+        <strong>Let's Talk!</strong>
+        <p>Wanna get in touch? Feel Free to contact with the buttons below or by using the
+            <router-link :to="{ name: 'Contact' }" class="link">
+                <strong>contact page!</strong>
+            </router-link>
+        </p>
+        <v-btn class="mx-4" icon :to="{ name: 'About' }">
+            <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-btn class="mx-4" icon href="mailto:zackcpetersen@gmail.com">
+            <v-icon>mdi-email</v-icon>
+        </v-btn>
+        <v-btn class="mx-4" icon href="tel:3854045953">
+            <v-icon>mdi-phone</v-icon>
+        </v-btn>
     </v-container>
 </template>
 

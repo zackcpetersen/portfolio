913c608aa03abc92596282419767779067cad952
zackckpetersen <zackcpetersen@gmail.com>
2022-04-08 10:53:12 -0600

    making prod backend endpoint https

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const development = process.env.NODE_ENV !== 'production'
-const baseUrl = development ? 'http://0.0.0.0/api/' : 'http://api.zackcpetersen.com/api/'
+const baseUrl = development ? 'http://0.0.0.0/api/' : 'https://api.zackcpetersen.com/api/'
 
 const axios = Axios.create({
     baseURL: baseUrl
